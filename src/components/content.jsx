9f718a8a68f75ad67fc5344a936a6db243fedbdf
Boyan Levchev <boyanlevchev@gmail.com>
2020-09-14 16:16:43 +0100

    Added work in progress

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -20,7 +20,7 @@ function Content(props) {
           <ul id="links">
             <li id="muse-li"><a href="https://www.musevr.art" target="_blank"><h2 className="h2-left">> Muse VR</h2><h2 className="h2-right">A DIY virtual reality gallery builder for artists and curators</h2></a></li>
             <li id="froala-li"><a href="https://froala.com/wysiwyg-editor/whiteboard" target="_blank"><h2 className="h2-left">> Froala Whiteboard</h2><h2 className="h2-right">A collaborative brainstorming tool</h2></a></li>
-            <li id="violeta-li"><a href="http://violeta-popov-demo.herokuapp.com" target="_blank"><h2 className="h2-left">> Violeta Popov M.D.</h2><h2 className="h2-right">A portfolio website for an NY-based medical practitioner</h2></a></li>
+            <li id="violeta-li"><a href="http://violeta-popov-demo.herokuapp.com" target="_blank"><h2 className="h2-left">> Violeta Popov M.D.</h2><h2 className="h2-right">A portfolio website for an NY-based medical practitioner - <br/> work in progress...</h2></a></li>
             <div className="muse-1 muse-images"></div>
             <div className="muse-2 muse-images"></div>
             <div className="violeta-image violeta-1"></div>
