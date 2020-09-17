b40f87a48d75b41782c346cbf625591de663aefb
Boyan Levchev <boyanlevchev@gmail.com>
2020-09-17 10:29:36 +0100

    added a redirect for heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+import sslRedirect from 'heroku-ssl-redirect';
+
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(sslRedirect());
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
